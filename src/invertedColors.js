be71792385faef4ce2089fb41944758dbb3ac1c6
Rishabh Rao <rishabh.ink+github@gmail.com>
2021-07-15 11:43:46 +0200

    Use @magica11y/match-user-preference which has zero-dependencies and use @magica11y/cauldron only for development

--- src/invertedColors.js
+++ src/invertedColors.js
@@ -1,6 +1,6 @@
 // @flow
 
-import matchUserPreference from '@magica11y/cauldron';
+import matchUserPreference from '@magica11y/match-user-preference';
 
 import colorPreferences, { type ColorPreference } from './colorPreferences';
 
